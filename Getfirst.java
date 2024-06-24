ad100fee5daf3f169a3fb7c79ab73a833ab92175
anitha6006 <166504421+anitha6006@users.noreply.github.com>
2024-06-24 16:40:40 +0530

    get

---
+++ Getfirst.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+class Getfirst{
+    public static void main(String[] args){
+        LinkedList<String> colors=new LinkedList<>();
+        colors.add("red");
+        colors.add("blue");
+        colors.add("yellow");
+        colors.add("black");
+        System.out.println(colors.getFirst());
+    }
+}
+    
+
