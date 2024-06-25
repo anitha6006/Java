16abf78de6a45b5642dccef2f0e7eff1143bac66
anitha6006 <166504421+anitha6006@users.noreply.github.com>
2024-06-25 16:55:18 +0530

    addelement

---
+++ add.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+class add{
+    public static void main(String[] args){
+        LinkedList<String> colors=new LinkedList<>();
+        colors.add("blue");
+        colors.add("red");
+        colors.add("white");
+        colors.add("yellow");
+        colors.add(3,"black");
+        System.out.println(colors);
+    }
+}  
+    
+
